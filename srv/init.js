3094eb6e4ae46cdeb3a3a627285b74888e6ee92e
Gregor Wolf <gregor.wolf@gmail.com>
2020-08-26 18:59:44 +0200

    V2 proxy, Customer Material, Product

--- srv/init.js
+++ srv/init.js
@@ -3,6 +3,8 @@ var BusinessPartnerAddress = require("./data/BusinessPartnerAddress.json");
 var Customer = require("./data/Customer.json");
 var CustomerSalesArea = require("./data/CustomerSalesArea.json");
 var CustomerCompany = require("./data/CustomerCompany.json");
+var Product = require("./data/Product.json");
+var CustomerMaterial = require("./data/CustomerMaterial.json");
 
 module.exports = (db)=>{
   const { 
@@ -14,11 +16,26 @@ module.exports = (db)=>{
   } = db.entities(
     'API_BUSINESS_PARTNER'
   )
+
+  const { 
+    A_Product 
+  } = db.entities(
+    'API_PRODUCT_SRV'
+  )
+
+  const { 
+    A_CustomerMaterial 
+  } = db.entities(
+    'API_CUSTOMER_MATERIAL_SRV'
+  )
+
   return cds.run ([
     INSERT.into(A_BusinessPartner).entries(BusinessPartner),
     INSERT.into(A_BusinessPartnerAddress).entries(BusinessPartnerAddress),
     INSERT.into(A_Customer).entries(Customer),
     INSERT.into(A_CustomerSalesArea).entries(CustomerSalesArea),
     INSERT.into(A_CustomerCompany).entries(CustomerCompany),
+    INSERT.into(A_Product).entries(Product),
+    INSERT.into(A_CustomerMaterial).entries(CustomerMaterial),
   ])
 }
